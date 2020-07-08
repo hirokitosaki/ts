aef557e830748d58dfef55645e8795e7da747274
htosaki <github@hirokitosaki.me>
2020-07-08 23:09:24 +0900

    day01 型定義のサンプルコードを書いた

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('test');
+type IdDisplay = {
+  id: string;
+  display: string;
+};
+
+const list: IdDisplay[] = [
+  {
+    id: "foo",
+    display: "foo select"
+  },
+  {
+    id: "bar",
+    display: "bar select"
+  },
+  {
+    id: "num",
+    display: "num select"
+  }
+];
+
+const fooIndex = list.map(i => i.id).indexOf("num");
+console.log(fooIndex);
